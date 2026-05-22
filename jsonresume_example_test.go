e1127f3855e41a7d0e263bf652c9391848042f10
Charles Iliya Krempeaux <iamreiver@gmail.com>
2026-05-21 22:45:08 -0700

    example

---
+++ jsonresume_example_test.go
@@ -0,0 +1,27 @@
+package jsonresume_test
+
+import (
+	"fmt"
+
+	"github.com/reiver/go-json"
+
+	"github.com/reiver/go-jsonresume"
+)
+
+func ExampleJSONResume() {
+
+	var obj jsonresume.JSONResume
+
+	obj.Resume = append(obj.Resume, jsonresume.ResumeID("http://example.com/resume/executive"))
+	obj.Resume = append(obj.Resume, jsonresume.ResumeID("http://example.com/resume/programmer"))
+
+	bytes, err := json.Marshal(obj)
+	if nil != err {
+		fmt.Println("ERROR:", err)
+		return
+	}
+	fmt.Println(string(bytes))
+
+	// Output:
+	// {"resume":["http://example.com/resume/executive","http://example.com/resume/programmer"]}
+}
