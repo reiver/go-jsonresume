d793ee236b39963b77e0589a9649715e683dd68c
Charles Iliya Krempeaux <iamreiver@gmail.com>
2026-05-27 00:04:16 -0700

    docs

---
+++ doc.go
@@ -0,0 +1,4 @@
+/*
+Package jsonresume provides tools for working with JSON Resume including JSON Resume expressed as JSON-LD (i.e., FEP-6158), for the Go programming language.
+*/
+package jsonresume
